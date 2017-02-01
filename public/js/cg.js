c0c1f522f23e427402bc9b0de283786b16284e7c
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2017-02-01 18:21:58 +0000

    Loads of changes like working clock, colour scheme, flexible gradients

--- public/js/cg.js
+++ public/js/cg.js
@@ -1,7 +1,7 @@
 var app = angular.module('cgApp', ['socket-io', 'ngAnimate']);
 
-app.controller('generalCtrl', ['$scope', '$timeout', 'socket',
-    function($scope, $timeout, socket){
+app.controller('generalCtrl', ['$scope', '$timeout', '$interval', '$filter', 'socket',
+    function($scope, $timeout, $interval, $filter, socket){
         $scope.tickInterval = 1000;
 
         socket.on("general", function (state) {
@@ -18,10 +18,19 @@ app.controller('generalCtrl', ['$scope', '$timeout', 'socket',
             socket.emit("general:get");
         }
 
+        $scope.colonOnBool = true;
+
         var tick = function () {
             $scope.clock = Date.now();
+            $scope.colonOnBool = !$scope.colonOnBool;
             $timeout(tick, $scope.tickInterval);
         };
+
+        $scope.liveToggle = true;
+
+        $interval(function () {
+            $scope.liveToggle = !$scope.liveToggle;
+        }, 10000);
 
         $timeout(tick, $scope.tickInterval);
     }
